05e3efcb5502ca5039d42da95e5a4c30c7f988ba
palmern22 <palmern22@gmail.com>
2013-08-09 20:31:38 -0400

    Initial Commit

---
+++ HomeControl/HomeControl/Controllers/ControlController.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using SoapBox.FluentDwelling;
+using SoapBox.FluentDwelling.Devices;
+using HomeControl.Modem;
+namespace HomeControl.Controllers
+{
+    public class ControlController : Controller
+    {
+        //
+        // GET: /Control/
+        public string AcAddress = "22.C0.EC";
+        //public Plm modem = new Plm("COM6");
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult DeviceList()
+        {
+            var database = ModemManager.modem.GetAllLinkDatabase();
+            foreach (var record in database.Records)
+            {
+                DeviceBase device;
+            }
+        }
+
+        // GET: /TurnOff/
+
+        public ActionResult TurnOff()
+        {
+            DeviceBase device;
+            if (ModemManager.modem.Network.TryConnectToDevice(AcAddress, out device))
+            {
+                var ac = device as LightingControl;
+                ac.TurnOff();
+            }
+
+            return new EmptyResult();
+        }
+        public ActionResult TurnOn()
+        {
+            DeviceBase device;
+            if (ModemManager.modem.Network.TryConnectToDevice(AcAddress, out device))
+            {
+                var ac = device as LightingControl;
+                ac.TurnOn();
+            }
+
+            return new EmptyResult();
+        }
+
+    }
+}
---
+++ HomeControl/HomeControl/Modem/ModemManager.cs
@@ -0,0 +1,13 @@
+﻿using SoapBox.FluentDwelling;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace HomeControl.Modem
+{
+    public static class ModemManager
+    {
+        public static Plm modem = new Plm("COM6");
+    }
+}
